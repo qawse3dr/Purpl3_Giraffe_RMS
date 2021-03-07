444b888e13a5785ac84cdfb25db8936dfec4faca
qawse3dr <lawrencemilne38@gmail.com>
2021-03-06 23:50:15 -0500

    Req22 Pref Manager     Adds a preferences manager used to hold basic config like port     adds skeleton functions for endpoint operations
        Adds error codes which can be retrived from pref manager

---
+++ libpurpl3/login.py
@@ -0,0 +1,36 @@
+from flask import jsonify
+import libpurpl3.preferences as pref 
+
+def login(data: dict) -> str:
+  
+  return jsonify(
+    Error = {
+          "code":pref.Success.code,
+          "str": str(pref.Success)
+          },
+    data = {
+      "Success": True
+    }
+  )
+
+def manageUser(data: dict) -> str:
+  return jsonify(
+    Error = {
+          "code":pref.Success.code,
+          "str": str(pref.Success)
+          },
+    data = {
+      "Success": True
+    }
+  )
+
+def changePassword(data: dict) -> str:
+  return jsonify(
+    Error = {
+          "code":pref.Success.code,
+          "str": str(pref.Success)
+          },
+    data = {
+      "Success": True
+    }
+  )
