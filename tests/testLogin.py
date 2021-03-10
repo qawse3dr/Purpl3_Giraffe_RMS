59c793f5a228fdd5e6f164b924e8fe02678dec72
JamesCodes <buschj@uoguelph.ca>
2021-03-10 04:27:22 -0500

    added another test case

--- tests/testLogin.py
+++ tests/testLogin.py
@@ -1,7 +1,7 @@
-from flask import jsonify
-from cryptography.fernet import Fernet
 import unittest
 import libpurpl3.login as login
+from flask import jsonify
+from cryptography.fernet import Fernet
 
 class BaseTestCase(unittest.TestCase):
     def testPasswordEncryption(self):
@@ -10,6 +10,15 @@ class BaseTestCase(unittest.TestCase):
         holder = cipher_suite.encrypt("THIS_1S-P0ggers".encode())
         holder2 = login.encryptPassword("THIS_1S-P0ggers", key)
         self.assertEqual(holder, holder2)
+    
+    def testPasswordDecryption(self):
+        key = Fernet.generate_key()
+        cipher_suite = Fernet(key)
+        holder = login.encryptPassword("THIS_1S-P0ggers", key)
+        self.assertEqual("THIS_1S-P0ggers".encode(), cipher_suite.decrypt(holder))
+        
+
+    
 
 
 
