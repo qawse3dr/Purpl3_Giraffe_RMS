bd2948ab05da3a1bd506603c3123902b3d978fdd
Daniela Aldave-Garza <daldaveg@uoguelph.ca>
2021-03-16 13:51:30 -0400

    Added base pages

---
+++ purpl3_rms/src/pages/ScriptLogs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ScriptLogs extends React.Component {
+    render() {
+        return (
+            <h1>Script Logs!!</h1>
+        );
+    }
+}
+
+export default ScriptLogs
