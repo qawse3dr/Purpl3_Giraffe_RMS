e8455d00e370ac31162d987b1f78a9484c52b452
julianbooleanlau <julianlau2000@gmail.com>
2021-03-23 14:58:43 -0400

    Added some testing code.

--- purpl3_rms/src/components/run_script_page/run_script_page_soory.js
+++ purpl3_rms/src/components/run_script_page/run_script_page_soory.js
@@ -1,44 +1,56 @@
 import './run_script_page.css';
 import axios from "axios";
 import Table from '../table/table_soory.js';
+import LiveOutput from './LiveOutput.js';
 
 const RunScriptPage = (props) => {
     return (
         <div>
             <div className="body">
                 <div className="column">
-                <h1>Select Computer</h1>
-                <div className="scroll">
-                    <Table input={[{name:'soory\'s computer\tIp:123.1234.7777',script:Select_computer_func},{name:'tom\'s computer     Ip:123.1234.7777',script:Select_computer_func},{name:'jeary\'s computer     Ip:123.1234.7777',script:Select_computer_func}]}/>
+                    <h1>Select Computer</h1>
+                    <div className="scroll">
+                        <Table input={[{name:'soory\'s computer\tIp:123.1234.7777',script:Select_computer_func},{name:'tom\'s computer     Ip:123.1234.7777',script:Select_computer_func},{name:'jeary\'s computer     Ip:123.1234.7777',script:Select_computer_func}]}/>
+                    </div>
                 </div>
-                </div>
+
                 <div className="column">
-                <h1>Select Script</h1>
-                <div className="scroll">
-                    <Table input={[{name:'script_1',script:Select_script_func},{name:'diamond hands script',script:Select_script_func},{name:'monkey see monkey do',script:Select_script_func}]}/>
+                    <h1>Select Script</h1>
+                    <div className="scroll">
+                        <Table input={[{name:'script_1',script:Select_script_func},{name:'diamond hands script',script:Select_script_func},{name:'monkey see monkey do',script:Select_script_func}]}/>
+                    </div>
                 </div>
-                </div>
+
                 <p id="Select_Computer_text"></p>
                 <p id="Select_Script_text"></p>
             </div>
+
             <footer>
                 <div className="Run_Button">
-                    <button type="button" onClick={Run_script}>sadf</button>
+                    <button type="button" onClick={Run_script}>Run Script</button>
                 </div>
+
+                <button type="button" onClick={Update}>Test on main page</button>
+                <LiveOutput test={5}/>
             </footer>
         </div>
     )
 }
-  
+
+function Update() {
+    let scriptID = document.getElementById("Select_Script_text").value;
+    alert(scriptID);
+}
+
 function Select_computer_func(parms){
     let text = document.getElementById("Select_Computer_text");
-    text.textContent = "Selected Computer : "+ parms;
+    text.textContent = "Selected Computer : " + parms;
     text.value = parms;
 }
 
 function Select_script_func(parms){
     let text = document.getElementById("Select_Script_text");
-    text.textContent = "Selected Script : "+parms;
+    text.textContent = "Selected Script : " + parms;
     text.value = parms;
 }
 
--- purpl3_rms/src/pages/RunScript.js
+++ purpl3_rms/src/pages/RunScript.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import RunScriptPage from '../components/run_script_page/run_script_page_soory.js'
+import LiveOutput from '../components/run_script_page/LiveOutput.js'
 
 class RunScripts extends React.Component {
     render() {
+    	let script = document.getElementById("Select_Computer_text");
         return (
-            <RunScriptPage/>
+        	<div>
+            	<RunScriptPage/>
+            </div>
         );
     }
 }
