6e3ac9b13b198af9b7577e1d1a1b6bbadb4aaf95
julianbooleanlau <julianlau2000@gmail.com>
2021-03-23 00:37:37 -0400

    Live output ready to accept input

--- purpl3_rms/src/pages/RunScript.js
+++ purpl3_rms/src/pages/RunScript.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import LiveOutput from '../components/LiveOutput.js'
 
 class RunScripts extends React.Component {
+	constructor() {
+	    super();
+	    this.state = {
+	      currentTab: "Run Script"
+	    }
+  	}
+
     render() {
         return (
-            <h1>Run Scripts Tabs!!</h1>
+        	<div>
+        		<h1>Run Scripts Tabs!</h1>
+           		<p>Soory add yo shit here</p>
+           		<LiveOutput script="Aids" computer="Monkey"/>
+            </div>
         );
     }
 }
