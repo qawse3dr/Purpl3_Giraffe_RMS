cba406fe19f53a219e4bb66ae05b693be5d75f66
Daniela Aldave-Garza <daldaveg@uoguelph.ca>
2021-03-25 19:07:13 -0400

    Added Login Functionality

--- purpl3_rms/src/pages/Login.js
+++ purpl3_rms/src/pages/Login.js
@@ -1,12 +1,57 @@
 import React from 'react'
-import LoginForm from '../components/LoginForm'
+import './Login.css'
+import logo from '../res/logo.png'
+import axios from 'axios'
 
-class InputField extends React.Component {
+class Login extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
     render() {
         return (
-            <LoginForm history={this.props.history}/>
+        <div className="login-page">
+            <div className="login-block">
+                <img className="logo-img" src={logo}/>
+                <p id="invalidCreds" style={{color: "red"}}></p>
+                <label>Username</label>
+                <input id = "username" type="text"/><br/>
+                <label>Password</label>
+                <input id = "password" type="password"/><br/><br/>
+                <button className="btn" onClick={this.handleLogin}>Login</button>
+            </div>
+        </div>
         );
     }
+
+    handleLogin = () => {
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+
+        axios.post("/login", {
+            body: {
+                op: "LOGIN",
+                data: {
+                    Username: username,
+                    Password: password
+                }
+            }
+            }).then((res) => {
+              alert(JSON.stringify(res.data))
+              //Update login status
+              if (res.data == 0){
+                  this.props.sendLoginStatus(true);
+                  this.props.history.push("/run-script")
+              } 
+              else{//else -> invalid credentials, inform user
+                document.getElementById("invalidCreds").innerHTML="Invalid username or password"
+              }
+            }).catch((res) =>{
+              alert("Post Failed")
+            })
+    }
+
 }
 
-export default InputField
+export default Login
