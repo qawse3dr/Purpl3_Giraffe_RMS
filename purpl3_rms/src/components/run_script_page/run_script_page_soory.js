7ccf9a4d01287f85185916bbbd84c996c3be53e4
julianbooleanlau <julianlau2000@gmail.com>
2021-03-27 17:48:38 -0400

    Live output done, not tested with working post.

--- purpl3_rms/src/components/run_script_page/run_script_page_soory.js
+++ purpl3_rms/src/components/run_script_page/run_script_page_soory.js
@@ -2,51 +2,124 @@ import './run_script_page.css';
 import axios from "axios";
 import Table from '../table/table_soory.js';
 import LiveOutput from './LiveOutput.js';
+import React from "react";
 
-const RunScriptPage = (props) => {
-    return (
-        <div>
-            <div className="body">
-                <div className="column">
-                    <h1>Select Computer</h1>
-                    <div className="scroll">
-                        <Table input={[ {name:'Soory\'s computer\tIp:123.1234.7777',script:Select_computer_func},
-                                        {name:'Larry\'s computer\tIp:123.1234.7777',script:Select_computer_func},
-                                        {name:'Julian\'s computer\tIp:123.1234.7777',script:Select_computer_func},
-                                        {name:'Rachael\'s computer\tIp:123.1234.7777',script:Select_computer_func},
-                                        {name:'Daniela\'s computer\tIp:123.1234.7777',script:Select_computer_func},
-                                        {name:'James\'s computer\tIp:123.1234.7777',script:Select_computer_func}]}/>
+class RunScriptPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            runningScript: -1
+        }
+
+        this.handleRunScript = this.handleRunScript.bind(this);
+        this.handleLiveOutput = this.handleLiveOutput.bind(this);
+    }
+
+    handleRunScript(){
+        let in_computer = document.getElementById("Select_Computer_text");
+        let in_script = document.getElementById("Select_Script_text");
+
+        axios.post("/api", {
+            body: {
+              op:"RUN_SCRIPT",
+              data:{
+                ScriptID: in_script.value,
+                ComputerID: in_computer.value
+              }
+            }
+        }).then((res) => {
+            alert(JSON.stringify(res.data))
+            let btn = document.getElementById("run_page_runbtn");
+            btn.value = res.data;
+            console.log(btn.value.Id);
+        
+            //Updating the currently running script to be the return of this function call
+            this.setState(state => ({
+                runningScript: res.data.id
+            }));
+
+        }).catch((res) =>{ //Script failed
+            alert("Post Failed"); //Prob remove this final ver
+
+            //Update the currently running script to be -1 or nothing
+            this.setState(state => ({
+                runningScript: -1
+            }));
+            document.getElementById("Live_Output").value = "Running the script failed. Try again";
+        })
+    }
+
+    //Call to backend to retrieve script output.
+    handleLiveOutput(parms){
+        //Only run if the is a script actually running.
+        if (this.state.runningScript != -1) {
+            axios.post("/api", {
+                body: {
+                    op:"GET_FILE",
+                    data:{
+                        Id: parms,
+                        Filetype: "ENUM"
+                    }
+                }
+            }).then((res) => {
+                alert(JSON.stringify(res.data))
+
+                //Set the live output textarea
+                document.getElementById("Live_Output").value = res.data.entry
+            }).catch((res) =>{
+                alert("Post Failed")
+                document.getElementById("Live_Output").value = "Something went wrong with getting the live output :C. Please try again."
+            })
+        } else {
+            document.getElementById("Live_Output").value = "Run a script first before checking the live output!"
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="body">
+                    <div className="column">
+                        <h1>Select Computer</h1>
+                        <div className="scroll">
+                            <Table input={[ {name:'Soory\'s computer\tIp:123.1234.7777',script:Select_computer_func},
+                                            {name:'Larry\'s computer\tIp:123.1234.7777',script:Select_computer_func},
+                                            {name:'Julian\'s computer\tIp:123.1234.7777',script:Select_computer_func},
+                                            {name:'Rachael\'s computer\tIp:123.1234.7777',script:Select_computer_func},
+                                            {name:'Daniela\'s computer\tIp:123.1234.7777',script:Select_computer_func},
+                                            {name:'James\'s computer\tIp:123.1234.7777',script:Select_computer_func}]}/>
+                        </div>
                     </div>
+
+                    <div className="column">
+                        <h1>Select Script</h1>
+                        <div className="scroll">
+                            <Table input={[ {name:'Shutdown Select_Computer_text',script:Select_script_func},
+                                            {name:'Install Libre Office',script:Select_script_func},
+                                            {name:'Send meeting email',script:Select_script_func},
+                                            {name:'Order apple juice',script:Select_script_func},
+                                            {name:'Toggle smart lights',script:Select_script_func},
+                                            {name:'Cancel apple juice',script:Select_script_func}]}/>
+                        </div>
+                    </div>
+
+                    <p id="Select_Computer_text"></p>
+                    <p id="Select_Script_text"></p>
                 </div>
 
-                <div className="column">
-                    <h1>Select Script</h1>
-                    <div className="scroll">
-                        <Table input={[ {name:'Shutdown Select_Computer_text',script:Select_script_func},
-                                        {name:'Install Libre Office',script:Select_script_func},
-                                        {name:'Send meeting email',script:Select_script_func},
-                                        {name:'Order apple juice',script:Select_script_func},
-                                        {name:'Toggle smart lights',script:Select_script_func},
-                                        {name:'Cancel apple juice',script:Select_script_func}]}/>
+                <footer>
+                    <div className="Run_Button">
+                        <button type="button" onClick={this.handleRunScript}>Run Script</button>
+                        <button type="button" onClick={this.handleLiveOutput}>Refresh Output</button>
                     </div>
-                </div>
-
-                <p id="Select_Computer_text"></p>
-                <p id="Select_Script_text"></p>
+                    
+                    <textarea id="Live_Output" readonly rows="10" cols="200">
+                        Run a program to view it's output!
+                    </textarea>
+                </footer>
             </div>
-
-            <footer>
-                <div className="Run_Button">
-                    <button type="button" onClick={Run_script}>Run Script</button>
-                    <button type="button" >Refresh Output</button>
-                </div>
-                
-                <textarea id="Live_Output" readonly rows="10" cols="200">
-                    Run a program to view it's output!
-                </textarea>
-            </footer>
-        </div>
-    )
+        );
+    }
 }
 
 function Select_computer_func(parms){
@@ -59,56 +132,6 @@ function Select_script_func(parms){
     let text = document.getElementById("Select_Script_text");
     text.textContent = "Selected Script : " + parms;
     text.value = parms;
-}
-
-function Run_script(parms){
-    let in_computer = document.getElementById("Select_Computer_text");
-    let in_script = document.getElementById("Select_Script_text");
-
-    axios.post("/api", {
-        body: {
-          op:"RUN_SCRIPT",
-          data:{
-            ScriptID: in_script.value,
-            ComputerID: in_computer.value
-          }
-        }
-    }).then((res) => {
-        alert(JSON.stringify(res.data))
-        let btn = document.getElementById("run_page_runbtn");
-        btn.value = res.data;
-        console.log(btn.value.Id);
-    
-        //Set the live output
-        alert(res.data.id); //Julian Testing, you can remove this later
-        setLiveOutput(res.data.id); //Call to set the live output.
-
-    }).catch((res) =>{
-        alert("Post Failed"); //Prob remove this final ver
-
-        //Set the live output (error)
-        document.getElementById("Live_Output").value = "Running the script failed. Try again";
-    })
-
-    //Call to backend to retrieve script output.
-    function setLiveOutput(parms){
-      axios.post("/api", {
-        body: {
-            op:"GET_FILE",
-            data:{
-                Id: parms,
-                Filetype: "ENUM"
-            }
-        }
-        }).then((res) => {
-            alert(JSON.stringify(res.data))
-
-            //Set the live output textarea
-            document.getElementById("Live_Output").value = res.data.entry
-        }).catch((res) =>{
-            alert("Post Failed")
-        })
-    }
 }
   
 
