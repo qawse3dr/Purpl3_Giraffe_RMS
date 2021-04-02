75c9da9e4739a57371b60bfc6325b827cffcfe10
julianbooleanlau <julianlau2000@gmail.com>
2021-04-02 14:08:19 -0400

    Run script fixes + add computer formatting + error doc updates

---
+++ purpl3_rms/src/pages/Computer.js
@@ -0,0 +1,120 @@
+import './Computer.css';
+import React from 'react';
+import axios from 'axios'
+
+class Computer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+
+        this.handleAddComputer = this.handleAddComputer.bind(this);
+    }
+
+    componentDidMount() {
+    	//alert("Computer mounted");
+
+    	// axios.post("/api", {
+     //        body: {
+     //            op:"MANAGE_COMPUTERS",
+     //            funcOP: "GET_ALL",
+     //            data:{}
+     //            }
+     //        }
+     //    }).then((res) => {
+     //        alert("Success");
+     //        for (let x in res.data.enties) {
+     //        	alert(x)
+     //        }
+     //    }).catch((res) =>{
+     //        alert("Post Failed")
+     //    })
+
+
+		//If Value2 == NULL, search for equals or contains value1
+		//If Value2 !=NULL, search for between value1 and value2 
+		//SortType will either be ascending or descending based on enum
+
+  	}
+
+    handleAddComputer() {
+        let name = document.getElementById("name");
+        let description = document.getElementById("description");
+        let username = document.getElementById("username");
+        let password = document.getElementById("password");
+        let ip = document.getElementById("ip");
+
+        let message = document.getElementById("message");
+
+        if (name.value != "" && description.value != "" && username.value != "" && password.value != "" && ip.value != "") {
+        	axios.post("/api", {
+            	body: {
+	                op:"MANAGE_COMPUTERS",
+	                funcOP: "ADD",
+	                data:{
+	                	Name: name.value,
+						Desc: description.value,
+						Username: username.value,
+						Password:  password.value,
+						IP: ip.value,
+						isAdmin: false
+	                }
+	            }
+	        }).then((res) => {
+	            alert("Success! Computer added.");
+	            message.innerHTML = "Success! Computer added.";
+
+	            //Clearing fields after successfully adding
+	            name.value = "";
+		        description.value = "";
+		        username.value = "";
+		        password.value = "";
+		        ip.value = "";
+
+	        }).catch((res) =>{
+	            alert("Post Failed");
+	            message.innerHTML = "Error: Failed to add computer.";
+	        })
+        } else {
+        	message.innerHTML = "Please fill in all the blanks!";
+        }
+        
+    }
+
+    render() {
+        return (
+            <div>
+            	<h1>Computer Manager</h1>
+
+            	<div class="addComputer">
+	            	<h2>Adding Computer</h2>
+	            	<p>
+		                <label>Computer Name</label><br/>
+		                <input class="textArea" id="name" type="text"/>
+		            </p>
+		            <p>
+		                <label>Computer Description</label><br/>
+		                <input class="textArea" id="description" type="text"/>
+	                </p>
+	                <p>
+		                <label>Computer Username</label><br/>
+		                <input class="textArea" id="username" type="text"/>
+	                </p>
+	                <p>
+		                <label>Computer Password</label><br/>
+		                <input class="textArea" id="password" type="password"/>
+	                </p>
+	                <p>
+		                <label>Computer IP</label><br/>
+		                <input class="textArea" id="ip" type="text"/>
+	                </p>
+	            </div>
+
+                <p id="message"></p>
+                <button className="btn" onClick={this.handleAddComputer}>Add Computer</button>
+            </div>
+        );
+    }
+}
+
+export default Computer;
--- purpl3_rms/src/pages/ErrorDoc.js
+++ purpl3_rms/src/pages/ErrorDoc.js
@@ -3,114 +3,203 @@ import React from 'react'
 
 class ErrorDoc extends React.Component {
 
-	parseJSONtoTable() {
-		let tableHTML = "";
-		let i = 0;
-		let tableData = JSON.parse('"code": 123,"errorName": "test","description": "testDesc","errorMessage": "testErrorMsg"');
-		//let tableData = JSON.parse('"code": 123,"errorName": "test","description": "testDesc","errorMessage": "testErrorMsg","code": 123,"errorName": "test",description": "testDesc","errorMessage": "testErrorMsg"]");
+	// parseJSONtoTable() 
+	// 	let tableHTML = "";
+	// 	let i = 0;
+	// 	let tableData = JSON.parse('"code": 123,"errorName": "test","description": "testDesc","errorMessage": "testErrorMsg"');
+	// 	//let tableData = JSON.parse('"code": 123,"errorName": "test","description": "testDesc","errorMessage": "testErrorMsg","code": 123,"errorName": "test",description": "testDesc","errorMessage": "testErrorMsg"]");
 
-	    tableHTML += "<table>";
-	    for (i in tableData) 
-	    	tableHTML += "<tr><td>" + tableData[i] + "</td></tr>";
+	//     tableHTML += "<table>";
+	//     for (i in tableData) 
+	//     	tableHTML += "<tr><td>" + tableData[i] + "</td></tr>";
 	    
-	    tableHTML += "</table>";
+	//     tableHTML += "</table>";
 
-	    return tableHTML;
-	}
+	//     return tableHTML;
+	// 
 
     render() {
         return (
         	<div>
-	            <table class="tableizer-table">
-    				<thead><tr class="tableizer-firstrow"><th>Code</th><th>Name</th><th>Description</th><th>Error Message</th></tr>
-    				</thead>
-    				<tbody>
-						<tr><td>0</td><td>SUCCESS</td><td>Returned: %d, no error has occurred</td><td>NULL</td></tr>
-						<tr><td>1</td><td>UNKNOWN_ERROR</td><td>Returned: %d, an unknown error has occurred %s</td><td>An Unexpected Error has occured</td></tr>
-						<tr><td>2</td><td>INVALID_INPUT</td><td>the input given doesnt work</td><td>invalid input expected expected_format</td></tr>
-						<tr><td>3</td><td>FILE_NOT_FOUND</td><td>Returned: %d, file %s was not found</td><td>&nbsp;</td></tr>
-						<tr><td>4</td><td>CANT_CREATE_FILE</td><td>Returned: %d, file %s could not be created</td><td>&nbsp;</td></tr>
-						<tr><td>5</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>6</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>7</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>8</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>9</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>10</td><td>INVLAID_REQUEST</td><td>Returned %d, invalid request %s</td><td>&nbsp;</td></tr>
-						<tr><td>11</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>12</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>13</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>14</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>15</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>16</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>17</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>18</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>19</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>20</td><td>ATTRIBUTE_NOT_FOUND</td><td>Returned: %d, Attribute %s could not be found.</td><td>&nbsp;</td></tr>
-						<tr><td>21</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>22</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>23</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>24</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>25</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>26</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>27</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>28</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>29</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>30</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>31</td><td>AUTHENICATION_ERROR</td><td>failed to login</td><td>Login attempt fail please check username and password</td></tr>
-						<tr><td>32</td><td>INVALID_USERNAME</td><td>Given username is invalid to use</td><td>&nbsp;</td></tr>
-						<tr><td>33</td><td>NOT_LOGGED_IN</td><td>Returned %d: not logged in</td><td>&nbsp;</td></tr>
-						<tr><td>34</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>35</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>36</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>37</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>38</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>39</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>40</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>41</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>42</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>43</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>44</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>45</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>46</td><td>CONNECTION_FAILED</td><td>connection could not be made to server</td><td>Connection to user_name@computer_name could not be made</td></tr>
-						<tr><td>47</td><td>CONNECTION_DROPPED</td><td>the connection ending when the script was running</td><td>Connection to computer_name was lost</td></tr>
-						<tr><td>48</td><td>Error in scipt</td><td>The script exited with a non 0 code</td><td>The Script failed with error code error Code</td></tr>
-						<tr><td>49</td><td>CANT_FIND_SSH_KEY</td><td>couldnt find the ssh key in PRIVATE_SSH_KEY_PATH</td><td>Returned %d: failed to find a valid ssh key at %s</td></tr>
-						<tr><td>50</td><td>SSH_AUTHENTICATION_FAILED</td><td>ssh connection couldnt be made due to autentication</td><td>Returned %d: failed authentication over ssh for user_name@computer_name </td></tr>
-						<tr><td>51</td><td>EMPTY_SSH_PUBLIC_KEY</td><td>SSH public key entry is empty in the config</td><td>Returned %d: PUBLIC_SSH_KEY_VALUE config is empty</td></tr>
-						<tr><td>52</td><td>SSH_PERMISSION_DENIED</td><td>Couldnt access folder</td><td>"Returned %d: permission denied accessing: %s"</td></tr>
-						<tr><td>53</td><td>SSH_FAILED_TO_EXECUTE_SCRIPT</td><td>Failed to execute the script (failed to connect/ lost connection)</td><td>"Returned %d: failed to execute script: %s"</td></tr>
-						<tr><td>54</td><td>SSH_SCRIPT_FAILED_WITH_ERROR_CODE</td><td>script ended with a non-zero error code</td><td>"Returned:%d, script failed with error code: %s"</td></tr>
-						<tr><td>55</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>56</td><td>ERROR_BLACKLISTED_COMMAND</td><td>Script contains blacklisted command</td><td>"Returned%d, script contained invalid command: %s"</td></tr>
-						<tr><td>57</td><td>EOR_BLACKLISTED_IP</td><td>Connection IP is blacklisted</td><td>"returned: %d, ip %s is blacklisted"</td></tr>
-						<tr><td>58</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>59</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>60</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>61</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>62</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>63</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>64</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>65</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>66</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>67</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>68</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>69</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>70</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>71</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>72</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>73</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>74</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>75</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>76</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>77</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>78</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>79</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>80</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>81</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>82</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>83</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>84</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr><td>85</td><td>&nbsp;</td><td>&nbsp;</td><td></td></tr>
+    			<table>
+					<thead>
+						<tr class="tableizer-firstrow"><th>Code</th><th>Name</th><th>Description</th><th>Error Message</th></tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th>0</th>
+							<th>SUCCESS</th>
+							<th>No Error. Everything works as expected</th>
+							<th>Returned: 0, no error has occurred.</th>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>UNKNOWN_ERROR</td>
+							<td>An error that hasnt been registar has occurred read error message to see what went wrong.</td>
+							<td>Returned: 1, an unknown error has occurred ERROR_MSG.</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>INVALID_INPUT</td>
+							<td>The given input is incorrect and should be correct for request to go though</td>
+							<td>Returned: 2, invalid input.</td>
+						</tr>
+						<tr>
+							<td>3</td>
+							<td>FILE_NOT_FOUND</td>
+							<td>Couldn't find a file in a given path, this can be due to incorrect path or maybe file doesn't exist</td>
+							<td>Returned: 3, file FILE_NAME was not found</td>
+						</tr>
+						<tr>
+							<td>4</td>
+							<td>CANT_CREATE_FILE</td>
+							<td>Can't create a file. This could be due to a path of a folder that doesn't exist, or the user the server is running under doesn't have permission to the location</td>
+							<td>Returned: 4, file FILE_NAME could not be created</td>
+						</tr>
+						<tr>
+							<td>10</td>
+							<td>INVALID_REQUEST</td>
+							<td>An invalid request was made to the API, This has to do with how to server is set up if this occurs please reach out to the devlopers for assistance</td>
+							<td>Returned: 10, invalid request REQUEST</td>
+						</tr>
+						<tr>
+							<td>20</td>
+							<td>ATTRIBUTE_NOT_FOUND</td>
+							<td>The Attributes given could not be found If this  occurs please reach out to the devlopers for assistance</td>
+							<td>Returned: 20, Attribute ATTR_NAME could not be found.</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>31</td>
+							<td>AUTHENICATION_ERROR</td>
+							<td>Failed to login to the given account please check your username and password</td>
+							<td>Returned: 31, failed login for USERNAME</td>
+						</tr>
+						<tr>
+							<td>32</td>
+							<td>INVALID_USERNAME</td>
+							<td>The given username is not valid please select a new one. username can not include spaces or semi-colons ";"</td>
+							<td>Return: 32, failed login due too invalid username</td>
+						</tr>
+						<tr>
+							<td>33</td>
+							<td>NOT_LOGGED_IN</td>
+							<td>Requests can not be made unless you are logged into an account please login because sending any request to the api</td>
+							<td>Returned 33: Not Logged In to user</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>46</td>
+							<td>CONNECTION_FAILED</td>
+							<td>Connection could not be made to the computer this ussally occures if sshd is not running or if the computer is offline.</td>
+							<td>Returned: 46, Connection to user_name@computer_name could not be made</td>
+						</tr>
+						<tr>
+							<td>49</td>
+							<td>CANT_FIND_SSH_KEY</td>
+							<td>Couldn't find the ssh key in PRIVATE_SSH_KEY_PATH This key should be created with rsa encryption using ssh-keygen</td>
+							<td>Returned 49: failed to find a valid ssh key at PATH_TOO_SSH_KEY</td>
+						</tr>
+						<tr>
+							<td>50</td>
+							<td>SSH_AUTHENTICATION_FAILED</td>
+							<td>SSH connection couldn't be made due to autentication check your username and password and try again.</td>
+							<td>Returned 50: failed authentication over ssh for user_name@computer_name </td>
+						</tr>
+						<tr>
+							<td>51</td>
+							<td>EMPTY_SSH_PUBLIC_KEY</td>
+							<td>SSH public key entry is empty in the config This should be the public key pair of your RSA ssh key</td>
+							<td>Returned 51: PUBLIC_SSH_KEY_VALUE config is empty</td>
+						</tr>
+						<tr>
+							<td>52</td>
+							<td>SSH_PERMISSION_DENIED</td>
+							<td>Couldnt access folder due to permission deined please select a different folder in config or change permissions to the folder. This may happen if the computer shuts off during launching of the script</td>
+							<td>Returned 52: permission denied accessing: FILE_NAME</td>
+						</tr>
+						<tr>
+							<td>53</td>
+							<td>SSH_FAILED_TO_EXECUTE_SCRIPT</td>
+							<td>Failed to execute the script (failed to connect/ lost connection)</td>
+							<td>Returned 53: failed to execute script: SCRIPT_NAME</td>
+						</tr>
+						<tr>
+							<td>54</td>
+							<td>SSH_SCRIPT_FAILED_WITH_ERROR_CODE</td>
+							<td>script ended with a non-zero error code</td>
+							<td>Returned:54, script failed with error code: ERROR_CODE</td>
+						</tr>
+						<tr>
+							<td>55</td>
+							<td>ERROR_SSH_CONNECTION_LOST</td>
+							<td>Lost connection to the ssh client without handshake</td>
+							<td>Returned 55: Connection to remote computer USERNAME@IP running script SCRIPT_NAME lost</td>
+						</tr>
+						<tr>
+							<td>56</td>
+							<td>ERROR_BLACKLISTED_COMMAND</td>
+							<td>Script contains blacklisted command. Ask you administator about what cmd can and cannot be used</td>
+							<td>Returned 56, script contained invalid command: BLOCKED_COMMAND</td>
+						</tr>
+						<tr>
+							<td>57</td>
+							<td>ERROR_BLACKLISTED_IP</td>
+							<td>Connection IP is blacklisted. Ask your administator about this</td>
+							<td>Returned: 57, ip IP_ADDRESS is blacklisted</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>66</td>
+							<td>ERROR_CREATE_SQLITE3_CONNECTION</td>
+							<td>SQL connection could not be made, This can happen if the file cannot be found or created</td>
+							<td>Returned: 66, connection to sqlite3 database failed when executing function FTN on table TABLE with message - \"MSG\"</td>
+						</tr>
+						<tr>
+							<td>67</td>
+							<td>ERROR_EXECUTE_SQLITE3_COMMAND</td>
+							<td>SQL command could not be execute based on input</td>
+							<td>Returned: 67, executing command of type TYPE failed on table TABLE with message - \"MSG\"</td>
+						</tr>
+						<tr>
+							<td>68</td>
+							<td>ERROR_NO_ID_PROVIDED</td>
+							<td>SQL Command requires an id to run and must be passed run again with an id</td>
+							<td>Returned: 68, executing command CMD on table TABLE requires ID but no ID was provided</td>
+						</tr>
+						<tr>
+							<td>69</td>
+							<td>ERROR_SQL_RETURN_CAST</td>
+							<td>SQL select query returns a set of values of which at least one of them could not be casted to the expected type i.e. the return type was unexpected.</td>
+							<td>Returned: 69, after executing command CMD there was an error casting an element of returned tuple to TYPE type - \"MSG\"</td>
+						</tr>
+						<tr>
+							<td>70</td>
+							<td>ERROR_SQL_RETURN_MISSING_ATTR</td>
+							<td>SQL command did not return the number of values expected. Either returned no values as no entry corresponding to given command exists or returns some number of values not equal to the expected number of values.</td>
+							<td>Returned: 70, after executing command CMD on table TABLE, only ATTR_GIVEN attributes were returned out of expected ATTR_EXPECTED</td>
+						</tr>
 					</tbody>
 				</table>
 			</div>
